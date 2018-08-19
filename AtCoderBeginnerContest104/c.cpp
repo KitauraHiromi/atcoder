8efcdcdccf72052f4926658aa8aa244d2562a75e
u-kita <kasumiga_eria394@yahoo.co.jp>
2018-08-19 17:19:56 +0900

    [add] ABC104C

---
+++ AtCoderBeginnerContest104/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_D 11
+#define INF 1<<30
+typedef long long ll;
+ll D, G, p[MAX_D], c[MAX_D];
+
+int  main(void){
+	cin >> D >> G;
+
+	for(ll i=0; i<D; i++){
+		cin >> p[i] >> c[i];
+	}
+
+	ll res = INF;
+	for(ll i=0; i<(1<<D); i++){
+		ll g = 0; ll n = 0;
+		for(ll j=0; j<D; j++){
+			if(i>>j&1){
+				g += p[j] * (j+1) * 100 + c[j];
+				n += p[j];
+			}
+		}
+
+		if(G <= g) res = min(res, n);
+		else{
+			for(ll j=0; j<D; j++){
+				if(!(i>>j&1) && G > g && G <= g + (j+1) * 100 * p[j]){
+					res = min(res, n + ll(ceil(double(G - g) / (j+1) / 100)));
+				}
+			}
+		}
+	}
+	cout << res << endl;
+}
