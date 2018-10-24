73c56bb9d6771da8556f7e15fac28659d7b65034
u-kita <kasumiga_eria394@yahoo.co.jp>
2018-10-24 23:28:38 +0900

    [modify] ABC 110 b, c AC

--- AtCoderBeginnersContest110/c.cpp
+++ AtCoderBeginnersContest110/c.cpp
@@ -9,13 +9,15 @@ int main(void){
 
 	vector<int> ns(n), nt(n);
 	rep(i, n){
-		if(ds.find(s[i]) != ds.end()) ds[s[i]] = i;
+		if(ds.find(s[i]) == ds.end()) ds[s[i]] = i;
 		ns[i] = ds[s[i]];
 	}
+
 	rep(i, n){
-		if(dt.find(t[i]) != dt.end()) dt[t[i]] = i;
+		if(dt.find(t[i]) == dt.end()) dt[t[i]] = i;
 		nt[i] = dt[t[i]];
 	}
+
 	if(ns == nt) cout << "Yes" << endl;
 	else cout << "No" << endl;
 	return 0;
