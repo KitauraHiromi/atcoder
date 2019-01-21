0069a72f4aa6397d6b617d4027ed0a93cbcdea74
KitauraHiromi <kasumiga_eria394@yahoo.co.jp>
2019-01-21 22:19:17 +0900

    [add, modify] add ABC116 WA modify dirname

--- AtCoderBeginnerContest116/b.cpp
+++ AtCoderBeginnerContest116/b.cpp
@@ -10,21 +10,22 @@ typedef long long ll;
 typedef pair<ll, ll> P;
 ll n;
 unordered_map<double, double> m;
-ll f1(ll n){
-    return n / 2;
-}
+// ll f1(ll n){
+//     return n / 2;
+// }
 
-ll f2(ll n){
-    return 3 * n + 1;
-}
+// ll f2(ll n){
+//     return 3 * n + 1;
+// }
 
 signed main(){
     ll s;
     cin >> s;
     m[s] = 1;
     rep(i, 1000000){
-        if(s % 2 == 0) s = f1(s);
-        else s = f2(s);
+        // if(s % 2 == 0) s = f1(s);
+        // else s = f2(s);
+        s = (s % 2) ? s / 2 : 3 * n + 1;
         if(m.find(s) != m.end()){
             cout << i + 2 << endl;
             break;
--- AtCoderBeginnerContest116/d.cpp
+++ AtCoderBeginnerContest116/d.cpp
@@ -5,11 +5,44 @@
 #include <vector>
 #define MAX_N 100001
 #define rep(i, n) for(ll i=0; i<n; i++)
+#define repq(i, k, n) for(ll i=k; i<n; i++)
+#define pb push_back
 using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> P;
-ll n;
+ll n, k;
+P s[MAX_N];
+vector<P> p, q; // 取ったダブリ寿司, 取っていない新規寿司
+set<ll> t; // 取った寿司の種類を記録
 
 signed main(){
+    cin >> n >> k;
+    rep(i, n){
+        ll t, d;
+        cin >> t >> d;
+        s[i] = P(d, t);
+    }
+    sort(s, s+n, greater<P>());
+    ll ts = 0;
+    rep(i, k){
+        ts += s[i].first;
+        if(t.find(s[i].second) != t.end()) p.pb(s[i]);
+        else t.insert(s[i].second);
+    }
+    reverse(p.begin(), p.end());
+    ll x = t.size();
+    repq(i, k, n){
+        if(t.find(s[i].second) != t.end());
+        else q.pb(s[i]);
+    }
+
+    ll ret = ts;
+    rep(i, min(p.size(), q.size())){
+        ret = max(ret, ts + x * x);
+        ts += q[i].first - p[i].first;
+        x++;
+    }
+    ret = max(ret, ts + x * x);
+    cout << ret << endl;
     return 0;
 }
