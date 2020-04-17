cf84dfbdbf700579565c632fb021c372939294d8
Kitaura Hiromi <hiromi394.1002@gmail.com>
2020-04-17 15:36:41 +0900

    [update] ABC162 d

--- ABC162/d.cpp
+++ ABC162/d.cpp
@@ -12,49 +12,21 @@ using P = pair<ll, ll>;
 const ll INF = 9e18;
 string s;
 ll n;
-ll rgbi[MAX_N][3], rgbk[MAX_N][3];
+ll rgb[3];
 
 signed main(){
     cin >> n >> s;
 
-    // set rgbi and rgbk
-    rgbi[1][0] = rgbi[1][1] = rgbi[1][2] = 0;
-    rgbk[1][0] = rgbk[1][1] = rgbk[1][2] = 0;
-    // j = 1 (1 <= j <= n-2)
-    if(s[0]=='R') rgbi[1][0]++;
-    if(s[0]=='G') rgbi[1][1]++;
-    if(s[0]=='B') rgbi[1][2]++;
-    for(ll k=2; k<n; k++){
-        if(s[k]=='R') rgbk[1][0]++;
-        if(s[k]=='G') rgbk[1][1]++;
-        if(s[k]=='B') rgbk[1][2]++;
-    }
-    // j > 1 (1 <= j <= n-2)
-    for(ll j=2; j<=n-2; j++){
-        rgbi[j][0] = rgbi[j-1][0];
-        rgbi[j][1] = rgbi[j-1][1];
-        rgbi[j][2] = rgbi[j-1][2];
-        if(s[j-1]=='R') rgbi[j][0]++;
-        if(s[j-1]=='G') rgbi[j][1]++;
-        if(s[j-1]=='B') rgbi[j][2]++;
+    // set rgb
+    rgb[0] = rgb[1] = rgb[2] = 0;
 
-        rgbk[j][0] = rgbk[j-1][0];
-        rgbk[j][1] = rgbk[j-1][1];
-        rgbk[j][2] = rgbk[j-1][2];
-        if(s[j]=='R') rgbk[j][0]--;
-        if(s[j]=='G') rgbk[j][1]--;
-        if(s[j]=='B') rgbk[j][2]--;
+    for(ll i=0; i<n; i++){
+        if(s[i]=='R') rgb[0]++;
+        if(s[i]=='G') rgb[1]++;
+        if(s[i]=='B') rgb[2]++;
     }
 
-    // for(ll j=1; j<=2; j++) cout << rgbi[j][0] << ' ' << rgbi[j][1] << ' ' << rgbi[j][2] << endl;
-    // for(ll j=1; j<=2; j++) cout << rgbk[j][0] << ' ' << rgbk[j][1] << ' ' << rgbk[j][2] << endl;
-
-    ll ret = 0;
-    for(ll j=1; j<=n-2; j++){
-        if(s[j] == 'R') ret += rgbi[j][1] * rgbk[j][2] + rgbi[j][2] * rgbk[j][1];
-        if(s[j] == 'G') ret += rgbi[j][2] * rgbk[j][0] + rgbi[j][0] * rgbk[j][2];
-        if(s[j] == 'B') ret += rgbi[j][0] * rgbk[j][1] + rgbi[j][1] * rgbk[j][0];
-    }
+    ll ret = rgb[0] * rgb[1] * rgb[2];
 
     for(ll j=1; j<=n-2; j++){
         for(ll i=0; i<j; i++){
